b79554ed7c6d1937a9527aed58b51eaf980b41fd
Halyson Sampaio <halyson.sampaio@gmail.com>
2020-04-30 00:05:46 -0300

    Iniciando api e services de processamento

---
+++ service_a/app.py
@@ -0,0 +1,55 @@
+import pika
+import time
+
+
+sleepTime = 2
+print(f"[*] Sleep de {sleepTime} segundos.")
+time.sleep(sleepTime)
+
+print('[*] Conectando no servidor  ...')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='127.0.0.1')
+)
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
+
+print('[*] Aguardando mensagens')
+
+
+def send_to_next(message):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='127.0.0.1')
+    )
+
+    channel = connection.channel()
+    # channel.queue_declare(queue='task_queue_b', durable=True)
+    channel.basic_publish(
+        exchange='events',
+        routing_key='event.calc',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        )
+    )
+    connection.close()
+
+
+def callback(ch, method, properties, body):
+    print(f"[*] Recebido: {body}")
+    cmd = body.decode()
+
+    if cmd == 'entregar':
+        send_to_next(cmd)
+        print("[+] Entregue")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    elif cmd == 'retentar':
+        print("[-] Retentativa")
+        ch.basic_reject(delivery_tag=method.delivery_tag, requeue=True)
+    else:
+        ch.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+        print("[ ] Rejeitada")
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+channel.start_consuming()
---
+++ service_b/app.py
@@ -0,0 +1,51 @@
+import pika
+import time
+from random import randint
+
+sleepTime = 2
+print(f"[*] Sleep de {sleepTime} segundos.")
+time.sleep(sleepTime)
+
+print("[*] Conectando no servidor  ...")
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='127.0.0.1'))
+channel = connection.channel()
+channel.queue_declare(queue='task_queue_b', durable=True)
+
+print("[*] Aguardando mensagens")
+
+
+def process_cmd(cmd):
+    time.sleep(5)
+    number = randint(0, 100)
+    if cmd == 20:
+        raise Exception("Erro")
+    elif number % 2:
+        print("[+] Par")
+        return True
+    else:
+        print("[-] Impar")
+        return False
+
+
+def callback(ch, method, properties, body):
+    print(f"[*] Recebido: {body}")
+    cmd = body.decode()
+
+    try:
+        response = process_cmd(cmd)
+        if response:
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        else:
+            # basic_nack tem o mesmo efeito, diferenca apenas semantica
+            ch.basic_reject(delivery_tag=method.delivery_tag, requeue=True)
+
+    except Exception as e:
+        ch.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+        print("[ ] Rejeitado")
+        pass
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue_b', on_message_callback=callback)
+channel.start_consuming()
